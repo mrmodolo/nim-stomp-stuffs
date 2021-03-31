4bdd24c54438ab039922a0f02ca1888c411c8dd4
Marcelo Módolo <marcelo.rmodolo@gmail.com>
2021-03-31 09:43:46 -0300

    Testando V1.2 e V1.1

--- src/utils.nim
+++ src/utils.nim
@@ -1,5 +1,6 @@
 import re
 
+
 let RE_ESCAPE_V12 = [
   (re"\r", "\\r"),
   (re"\n", "\\n"),
@@ -7,8 +8,23 @@ let RE_ESCAPE_V12 = [
   (re"\\", "\\\\")
   ]
 
-let RE_UNESCAPE_V12* = [
+
+let RE_UNESCAPE_V12 = [
   (re"\\r", "\r"),
+  (re"\\n", "\n"),
+  (re"\\c", ":"),
+  (re"\\\\", "\\")
+  ]
+
+
+let RE_ESCAPE_V11 = [
+  (re"\n", "\\n"),
+  (re":", "\\c"),
+  (re"\\", "\\\\")
+  ]
+
+
+let RE_UNESCAPE_V11 = [
   (re"\\n", "\n"),
   (re"\\c", ":"),
   (re"\\\\", "\\")
@@ -21,3 +37,19 @@ proc escapeV12*(unescaped: string): string =
 
 proc unescapeV12*(escaped: string): string =
   result = multiReplace(escaped, RE_UNESCAPE_V12)
+
+
+proc escapeV11*(unescaped: string): string =
+  result = multiReplace(unescaped, RE_ESCAPE_V11)
+
+
+proc unescapeV11*(escaped: string): string =
+  result = multiReplace(escaped, RE_UNESCAPE_V11)
+
+
+proc escapeV10*(unescaped: string): string =
+  result = unescaped
+
+
+proc unescapeV10*(escaped: string): string =
+  result = escaped
--- tests/tutils.nim
+++ tests/tutils.nim
@@ -1,11 +1,20 @@
 import unittest
 import utils  
 
-const UNESCAPED = "\n\r:\\"
-const ESCAPED = "\\n\\r\\c\\\\"
+const UNESCAPED_V12 = "\n\r:\\"
+const ESCAPED_V12 = "\\n\\r\\c\\\\"
 
-test "can escape":
-  check ESCAPED == utils.escapeV12(UNESCAPED)
+const UNESCAPED_V11 = "\n:\\"
+const ESCAPED_V11 = "\\n\\c\\\\"
 
-test "can unescape":
-  check UNESCAPED == utils.unescapeV12(ESCAPED)
+test "can escape STOMP V1.2":
+  check ESCAPED_V12 == utils.escapeV12(UNESCAPED_V12)
+
+test "can unescape STOMP V1.2":
+  check UNESCAPED_V12 == utils.unescapeV12(ESCAPED_V12)
+
+test "can escape STOMP V1.1":
+  check ESCAPED_V11 == utils.escapeV11(UNESCAPED_V11)
+
+test "can unescape STOMP V1.1":
+  check UNESCAPED_V11 == utils.unescapeV11(ESCAPED_V11)
