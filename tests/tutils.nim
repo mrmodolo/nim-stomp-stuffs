4765decee3ed595dfe9bb750600dce69bda6ddef
Marcelo Módolo <marcelo.rmodolo@gmail.com>
2021-03-31 16:48:03 -0300

    Nomeando os blocos

--- tests/tutils.nim
+++ tests/tutils.nim
@@ -17,18 +17,18 @@ const UNESCAPED_V11 = "\n:\\"
 const ESCAPED_V11 = "\\n\\c\\\\"
 
 # "can escape STOMP V1.2"
-block:
+block escapeV12:
   doAssert ESCAPED_V12 == utils.escapeV12(UNESCAPED_V12)
 
 # "can unescape STOMP V1.2"
-block:  
+block unescapeV12:  
   doAssert UNESCAPED_V12 == utils.unescapeV12(ESCAPED_V12)
 
 # "can escape STOMP V1.1"
-block:
+block escapeV11:
   doAssert ESCAPED_V11 == utils.escapeV11(UNESCAPED_V11)
 
 # "can unescape STOMP V1.1"
-block:
+block unescapeV11:
   doAssert UNESCAPED_V11 == utils.unescapeV11(ESCAPED_V11)
 
